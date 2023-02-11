a56691f772248f31c3bbdc6bffc37115d1fdeb17
tomasxwhite <tomas8olivera@gmail.com>
2023-02-11 13:00:13 -0300

    feat:add todo simple function

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+
+    <br>
+    <input type="text" v-model="inputData"/>
+    <button @click="onAddTextToList">Submit Text</button>
+    <br>
+    <p>{{ inputData }}</p>
+
+    <ul>
+      <li v-for="text in inputDataArr" :key="text">{{ text }}</li>
+    </ul>
   </div>
 </template>
 
@@ -8,6 +18,23 @@
 
 export default {
   name: 'App',
+
+  /*HERE I CAN DEFINE MY GLOBAL VARIABLES WITH THERE INITIALIZATION*/
+  data() {
+    return {
+      inputData: null,
+      inputDataArr: []
+    }
+  },
+
+  /**THIS IS AN OPTION FOR THE FUNCTIONS */
+  methods:{
+
+    /**THIS METHOD ADD INPUTDATA TO MY ARR */
+    onAddTextToList() {
+      this.inputDataArr.push(this.inputData)
+    }
+  },
   components: {
   }
 }
